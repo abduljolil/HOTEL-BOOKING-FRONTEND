4ca910de931c6fbf63ab3eb68f96ffb1a4a454ec
jalil56545 <jalil.mariya.31@gmail.com>
2023-11-08 09:29:46 +0600

    creted a dining section

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -17,7 +17,7 @@ const Offers = () => {
     },[])
 
     return (
-      <div className="my-10">
+      <div className=" bg-base-400 my-10">
         <h1 className=" text-center text-5xl py-10">PROMOTIONS & SPECIAL OFFERS</h1>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
             {
@@ -28,7 +28,7 @@ const Offers = () => {
           <h2 className="card-title text-3xl font-semibold">{offer.title}</h2>
           <p className="text-xl">{offer.description}</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">FIND OUT MORE</button>
+            <button className="btn btn-success">FIND OUT MORE</button>
           </div>
         </div>
       </div>
